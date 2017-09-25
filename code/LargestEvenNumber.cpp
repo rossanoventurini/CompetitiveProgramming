eeba275e78b618c9eac82cbccac40a67675bf8ba
Giulio Ermanno Pibiri <jeis90@gmail.com>
2017-09-25 11:40:20 +0200

    fix LargestEvenNumber.cpp

--- code/LargestEvenNumber.cpp
+++ code/LargestEvenNumber.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-<<<<<<< HEAD
 #include <cstring>   // for memmove
 
 // problem: http://practice.geeksforgeeks.org/problems/largest-even-number/0
@@ -45,12 +44,37 @@ void largest_even_number(std::string& str) {
     // ASCII codes for '0' to '9' span a consecutive range ([48..57])
     bool reverse = true;
     counting_sort<std::string, char>(str, str.size(), 10, '0', reverse);
-=======
-#include <vector>
 
->>>>>>> 497079b31c292c3e96226b52579adf7ec0e9f901
+    // determine the position of the smallest even number (if present)
+    uint64_t i = str.size() - 1;
+    while (str[i] % 2) {
+        if (!i) break;
+        --i;
+    }
 
-// problem: http://practice.geeksforgeeks.org/problems/largest-even-number/0
+    if (str[i] % 2 == 0) { // could not contain any even number
+        char x = str[i];
+        // shift left by 1
+        memmove(&str[i], &str[i + 1], str.size() - i - 1);
+        str.back() = x;
+    }
+}
+
+int main() {
+
+    int num_test_cases = 0;
+    std::cin >> num_test_cases;
+
+    std::string str;
+    for (int i = 0; i < num_test_cases; ++i) {
+        std::cin >> str;
+        largest_even_number(str);
+        std::cout << str << std::endl;
+        str.clear();
+    }
+
+    return 0;
+}
 
 int main() {
 
